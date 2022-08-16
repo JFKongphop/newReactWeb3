707e8b76a7ebdbb84cf9f13a300b190bc4954c32
JFKongphop <93483308+JFKongphop@users.noreply.github.com>
2022-08-16 22:03:54 +0700

    Delete app3 directory

--- app3/test-git/src/components/HelloExternal.js
+++
@@ -1,5 +0,0 @@
-function HelloExternal(){
-    return <h1>External component</h1>
-}
-
-export default HelloExternal
--- app3/test-git/src/data/dataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react"; // main data to use all component
-
-const DataContext = createContext()
-
-export default DataContext
